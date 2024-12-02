6dce0a33474e5753ea6fe373ea4d32f4d128fbd1
tylrdnns <68363565+tylrdnns@users.noreply.github.com>
2024-12-02 11:18:48 -0600

    Update aichat.nix

--- aichat.nix
+++ aichat.nix
@@ -7,4 +7,25 @@
                 neovim
                 tmux
         ];
+
+        networking.firewall = {
+                enable = true;
+                        allowedTCPPorts = [ 80 443 8000 ];
+                        allowedUDPPortRanges = [
+                                { from = 4000; to = 4007; }
+                                { from = 8000; to = 8010; }
+                        ];
+        };
+
+        systemd.network = {
+                enable = true;
+                networks."50-enp5s0" = {
+                        matchConfig.Name = "enp5s0";
+                        networkConfig = {
+                                DHCP = "ipv4";
+                                IPv6AcceptRA = true;
+                        };
+                        linkConfig.RequiredForOnline = "routable";
+                };
+        };
 }
